4041eb3aa26e1cdd29a53b4671ce2d4f59e60d49
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-04-28 00:24:52 -0400

    added css that changes the text of the nav bar

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,10 @@
 $(() => {
   $('.title').slideDown(4000)
   $('.name').slideDown(4000)
+  $('a').mouseover(function(){
+    $('a').css('color', 'white').css('text-shadow', '1px 1px 5px gray')
+  })
+    $('a').mouseout(function(){
+    $('a').css('color', 'black').css('text-shadow', 'none')
+  })
 })
